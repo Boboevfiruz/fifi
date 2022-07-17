411224aeb0326b323e8a8462e93bc52117a4c605
Boboevfiruz <108644045+Boboevfiruz@users.noreply.github.com>
2022-07-17 23:14:31 +0500

    javascript commit

---
+++ 1 week/3wanbe/task.js
@@ -0,0 +1,16 @@
+// let mynumber=prompt("Enter Number") 
+// S=Math.abs(""+mynumber)
+// console.log(S.length))
+ 
+// let str =prompt("some word");
+//function reverseString(str) {
+
+  //  return str.split("").reverse().join("");
+  
+  //}
+   
+  //reverseString("hello"); // olleh
+   
+ // let text="firuzboboev";
+//  let length=text.length;
+  //console.log(length)
